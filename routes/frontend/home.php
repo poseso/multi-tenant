e5f0d89e2b786dd3956d6e0beb9f0e2493420d76
poseso <carlosmsanchez@gmail.com>
2019-09-22 20:26:43 -0400

    added route domain for main site

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -10,7 +10,11 @@ use App\Http\Controllers\Frontend\User\DashboardController;
  * Frontend Controllers
  * All route names are prefixed with 'frontend.'.
  */
-Route::get('/', [HomeController::class, 'index'])->name('index');
+Route::domain('multi-tenant.local')->group(function () {
+    Route::get('/', [HomeController::class, 'index'])->name('index');
+});
+
+
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
