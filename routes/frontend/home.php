2510c1f7119370216dda8097d656cf240b185646
poseso <carlosmsanchez@gmail.com>
2019-09-22 21:01:56 -0400

    removed whitespace

--- routes/frontend/home.php
+++ routes/frontend/home.php
@@ -14,7 +14,6 @@ Route::domain('multi-tenant.local')->group(function () {
     Route::get('/', [HomeController::class, 'index'])->name('index');
 });
 
-
 Route::get('contact', [ContactController::class, 'index'])->name('contact');
 Route::post('contact/send', [ContactController::class, 'send'])->name('contact.send');
 
