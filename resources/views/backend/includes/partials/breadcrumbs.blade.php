8d29f542bb20f9d814a543975de12e14aede1d8c
poseso <carlosmsanchez@gmail.com>
2019-09-22 21:00:40 -0400

    updated breadcrumbs

--- resources/views/backend/includes/partials/breadcrumbs.blade.php
+++ resources/views/backend/includes/partials/breadcrumbs.blade.php
@@ -1,40 +1,31 @@
-@if ($breadcrumbs)
-<div style="background-color:#fdfdfd;" class="kt-subheader kt-grid__item" id="kt_subheader">
-    <div class="kt-subheader__main">
-        <h3 class="kt-subheader__title">
-            @foreach ($breadcrumbs as $breadcrumb)
-                @if ($loop->last)
-                    {{ $breadcrumb->title }}
-                @endif
-            @endforeach
-        </h3>
+@if (count($breadcrumbs))
+    <div style="background-color:#fdfdfd;" class="kt-subheader kt-grid__item" id="kt_subheader">
+        <div class="kt-subheader__main">
+            <a href="{{ route('admin.dashboard') }}" class="kt-subheader__breadcrumbs-link" data-toggle="kt-tooltip-desktop" data-skin="brand" title="{{ __('Pantalla Principal') }}" data-placement="bottom">
+                <i style="font-size: 2.0rem!important;" class="kt-subheader__breadcrumbs-link la la-home"></i>
+            </a>
 
-        <span class="kt-subheader__separator kt-subheader__separator--v"></span>
+            <h3 class="kt-subheader__title"></h3>
 
-        <a href="{{ route('admin.dashboard') }}" class="kt-subheader__breadcrumbs-link">
-            <i class="kt-subheader__breadcrumbs-link la la-home"></i>
-        </a>
+            <span class="kt-subheader__separator kt-subheader__separator--v"></span>
 
-        <h3 class="kt-subheader__title"></h3>
+            <div class="kt-subheader__title">
+                @foreach ($breadcrumbs as $breadcrumb)
+                    @if ($breadcrumb->url && !$loop->last)
+                        <a href="{{ $breadcrumb->url }}">
+                            <span data-toggle="kt-tooltip-desktop" data-skin="brand" title="{{ $breadcrumb->title }}" data-placement="bottom">{{ $breadcrumb->title }}</span>
+                        </a>
 
-        <span class="kt-subheader__separator kt-subheader__separator--v"></span>
-
-        <div class="kt-subheader__title">
-            @foreach ($breadcrumbs as $breadcrumb)
-                @if ($breadcrumb->url && !$loop->last)
-                    <a href="{{ $breadcrumb->url }}">
-                        <span>{{ $breadcrumb->title }}</span>
-                    </a>
-
-                    <span style="color:#cecece;">/</span>
-                @else
-                    <a class="kt-subheader__breadcrumbs-link">
-                        <span class="kt-subheader__title">{{ $breadcrumb->title }}</span>
-                    </a>
-                @endif
-            @endforeach
+                        <span style="color:#cecece;">&nbsp;<i style="color: #5867dd !important;" class="fa fa-angle-double-right"></i>&nbsp;</span>
+                    @else
+                        <a class="kt-subheader__breadcrumbs-link">
+                            <span class="kt-subheader__title">{{ $breadcrumb->title }}</span>
+                        </a>
+                    @endif
+                @endforeach
+            </div>
         </div>
+
+        @yield('breadcrumb-links')
     </div>
-    @yield('breadcrumb-links')
-</div>
 @endif
