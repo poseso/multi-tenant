68d390c362be48d03744fbb53256e985d41067bb
Carlos Sanchez <carlosmsanchez@gmail.com>
2019-10-09 15:28:58 -0400

    composer update

--- _ide_helper_models.php
+++ _ide_helper_models.php
@@ -14,19 +14,19 @@ namespace App\Models\Auth{
 /**
  * App\Models\Auth\Module
  *
+ * @property int $id
+ * @property string $name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Altek\Accountant\Models\Ledger[] $ledgers
  * @property-read int|null $ledgers_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Permission[] $permissions
- * @property-read int|null $permissions_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Role[] $roles
- * @property-read int|null $roles_count
- * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\User[] $users
- * @property-read int|null $users_count
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module newQuery()
- * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Permission\Models\Permission permission($permissions)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module query()
- * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Permission\Models\Permission role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Module whereUpdatedAt($value)
  */
 	class Module extends \Eloquent {}
 }
@@ -35,9 +35,19 @@ namespace App\Models\Auth{
 /**
  * Class PasswordHistory.
  *
+ * @property int $id
+ * @property int $user_id
+ * @property string $password
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\PasswordHistory whereUserId($value)
  */
 	class PasswordHistory extends \Eloquent {}
 }
@@ -46,9 +56,16 @@ namespace App\Models\Auth{
 /**
  * Class Permission.
  *
+ * @property int $id
+ * @property int $module_id
+ * @property string $name
+ * @property string|null $display_name
+ * @property string $guard_name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Altek\Accountant\Models\Ledger[] $ledgers
  * @property-read int|null $ledgers_count
- * @property-read \App\Models\Auth\Module $modules
+ * @property-read \App\Models\Auth\Module $module
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Permission[] $permissions
  * @property-read int|null $permissions_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Role[] $roles
@@ -60,6 +77,13 @@ namespace App\Models\Auth{
  * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Permission\Models\Permission permission($permissions)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission query()
  * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Permission\Models\Permission role($roles, $guard = null)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereDisplayName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereGuardName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereModuleId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Permission whereUpdatedAt($value)
  */
 	class Permission extends \Eloquent {}
 }
@@ -68,6 +92,12 @@ namespace App\Models\Auth{
 /**
  * Class Role.
  *
+ * @property int $id
+ * @property string $name
+ * @property string|null $description
+ * @property string $guard_name
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Altek\Accountant\Models\Ledger[] $ledgers
  * @property-read int|null $ledgers_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Permission[] $permissions
@@ -78,6 +108,12 @@ namespace App\Models\Auth{
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\Spatie\Permission\Models\Role permission($permissions)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereDescription($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereGuardName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\Role whereUpdatedAt($value)
  */
 	class Role extends \Eloquent {}
 }
@@ -86,11 +122,27 @@ namespace App\Models\Auth{
 /**
  * Class SocialAccount.
  *
+ * @property int $id
+ * @property int $user_id
+ * @property string $provider
+ * @property string $provider_id
+ * @property string|null $token
+ * @property string|null $avatar
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
  * @property-read \Illuminate\Database\Eloquent\Collection|\Altek\Accountant\Models\Ledger[] $ledgers
  * @property-read int|null $ledgers_count
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount query()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereProvider($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereProviderId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\SocialAccount whereUserId($value)
  */
 	class SocialAccount extends \Eloquent {}
 }
@@ -99,6 +151,28 @@ namespace App\Models\Auth{
 /**
  * Class User.
  *
+ * @property int $id
+ * @property string $uuid
+ * @property string|null $first_name
+ * @property string|null $last_name
+ * @property string $username
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $avatar_type
+ * @property string|null $avatar_location
+ * @property string|null $password
+ * @property \Illuminate\Support\Carbon|null $password_changed_at
+ * @property bool $active
+ * @property string|null $confirmation_code
+ * @property bool $confirmed
+ * @property string|null $timezone
+ * @property \Illuminate\Support\Carbon|null $last_login_at
+ * @property string|null $last_login_ip
+ * @property bool $to_be_logged_out
+ * @property string|null $remember_token
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property \Illuminate\Support\Carbon|null $deleted_at
  * @property-read string $full_name
  * @property-read string $name
  * @property-read string $permissions_label
@@ -116,7 +190,6 @@ namespace App\Models\Auth{
  * @property-read int|null $providers_count
  * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Auth\Role[] $roles
  * @property-read int|null $roles_count
- * @property-write mixed $password
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User active($status = true)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User confirmed($confirmed = true)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User newModelQuery()
@@ -125,6 +198,28 @@ namespace App\Models\Auth{
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User query()
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\BaseUser role($roles, $guard = null)
  * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\BaseUser uuid($uuid)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereActive($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereAvatarLocation($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereAvatarType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereConfirmationCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereConfirmed($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereLastLoginIp($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User wherePasswordChangedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereTimezone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereToBeLoggedOut($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereUsername($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Auth\User whereUuid($value)
  */
 	class User extends \Eloquent {}
 }
