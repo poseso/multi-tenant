0e3402737e0174a4b724dec6cd750f67452d4545
poseso <carlosmsanchez@gmail.com>
2019-10-19 13:41:59 -0400

    fixed social button title

--- resources/views/backend/auth/user/includes/social-buttons.blade.php
+++ resources/views/backend/auth/user/includes/social-buttons.blade.php
@@ -1,6 +1,6 @@
 @if ($user->providers->count())
     @foreach ($user->providers as $social)
-        <a href="{{ route( 'admin.auth.user.social.unlink', [$user, $social]) }}" data-toggle="kt-tooltip" data-skin="brand" data-placement="top" title="'.__('Desconectar').'" data-method="delete">
+        <a href="{{ route( 'admin.auth.user.social.unlink', [$user, $social]) }}" data-toggle="kt-tooltip" data-skin="brand" data-placement="top" title="{{ __('Desconectar') }}" data-method="delete">
             <i class="fab fa-{{ $social->provider }}"></i>
         </a>
     @endforeach
